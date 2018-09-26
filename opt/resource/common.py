ca5df738b1aafb842aacc0090bd8ec9b0d199d16
Luis Alvarez <luis.alvarez@kwhanalytics.com>
2018-09-26 13:17:36 -0700

    added output to stderr

--- opt/resource/common.py
+++ opt/resource/common.py
@@ -8,7 +8,7 @@ def get_payload():
     payload = json.load(sys.stdin)
     _, fname = tempfile.mkstemp()
     print("Logging payload to {}".format(fname), file=sys.stderr)
-    print("Payload: {}".format(payload))
+    print("Payload: {}".format(payload), file=sys.stderr)
     with open(fname, 'w') as fp:
         fp.write(json.dumps(payload))
     return payload
